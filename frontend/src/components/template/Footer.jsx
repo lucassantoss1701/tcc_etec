3a8f1000e77a1089130eb5c0811d5861dbf0d675
lucassantoss1701 <lucas.santoss1701@gmail.com>
2019-04-18 17:14:09 -0300

    first commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Footer.css';
+
+export default props =>
+    <footer className="footer">
+       <span>
+           Desenvolvido com o <i className="fa fa-heart text-danger"></i> por 
+           <strong> Alunos </strong>
+       </span>
+    </footer>
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import './Logo.css'
+import logo from '../../assets/images/logo.png';
+
+export default props => 
+    <aside className="logo">
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo"></img>
+        </Link>
+    </aside>
