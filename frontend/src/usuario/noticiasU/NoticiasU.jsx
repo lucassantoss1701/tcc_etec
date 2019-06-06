2a34fd69d99ab7c8991d4ca8285913c7b104491e
lucassantoss1701 <lucas.santoss1701@gmail.com>
2019-06-05 21:27:16 -0300

    upado

--- frontend/src/usuario/noticiasU/NoticiasU.jsx
+++ frontend/src/usuario/noticiasU/NoticiasU.jsx
@@ -5,62 +5,63 @@ import './divNoticias.css'
 import * as firebase from 'firebase'
 
 const now = new Date();
-var ru;
+let ru;
 
 export default class noticiasU extends Component {
-  constructor() {
-    super();
-    this.state = {
-      notices: [],
-    };
-    ru  = this.state.notices.map((notice, index) => <li key={index}>{notice.title}</li>);
-  }
-  componentDidMount() {
-    let ref = firebase.database().ref('/noticias');
-    ref.on('value', snapshot => {
-      const state = snapshot.val();
-      let thiz = this;
-      Object.getOwnPropertyNames(state).forEach(function(val, idx, array) {
-        console.log(state[val]);
-        thiz.state.notices.push(state[val]);
-      });
-      
-    });
-  }
+    constructor() {
+        super();
+        this.state = {
+            notices: [],
+        };
+        ru = this.state.notices.map((notice, index) => <li key={index}>{notice.title}</li>);
+    }
+    componentDidMount() {
+        let ref = firebase.database().ref('/noticias');
+        ref.on('value', snapshot => {
+            const state = snapshot.val();
+            let tempState = {
+                notices: []
+            };
+            Object.getOwnPropertyNames(state).forEach(function (val, idx, array) {
+                tempState.notices.push(state[val]);
+            });
+            console.log('temp', tempState);
+            this.setState(tempState);
+            console.log('state', this.state);
+            this.forceUpdate();
+        });
+    }
 
-
-
-  renderNotices() {
-
-      console.log(ru);
-      return (
-        
-        <ul>{ru}</ul>,
-        <div className="conteeiner">
-          <div className="caixaaa">
+    renderNotice(props, index) {
+        return <div key={index} className="caixaaa">
             <div className="imagem">
-              <img className="fotosss"
-                src={etec1}
-                alt="First slide"
-              />
-              <div className="resto">
-                <p>Título: {this.props.title}</p>
-                <p> Data: {now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear()} </p>
-              </div>
-              <button type="button" id="btn" className="btn btn-primary"> Ver mais!</button>
+                <img className="fotosss"
+                    src={etec1}
+                    alt="First slide"
+                />
+                <div className="resto">
+                    <p>Título: {props.title}</p>
+                    <p> Data: {now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear()} </p>
+                </div>
+                <button type="button" id="btn" className="btn btn-primary"> Ver mais!</button>
             </div>
-
-          </div>
         </div>
-      )
-  }
+    }
 
-  render() {
-    return (
-      <Main>
-        <h1>{this.notices}</h1>
-        {this.renderNotices()}
-      </Main>
-    )
-  }
+    renderNotices() {        
+        return (
+            <div className="conteeiner">
+                { this.state.notices.map((notice, index) => this.renderNotice(notice, index)) }                
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <Main>
+                <h1>{this.notices}</h1>
+                {this.renderNotices()}
+            </Main>
+        )
+    }
 }
